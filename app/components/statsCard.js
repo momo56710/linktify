e429ee3540b2b23688663d9fa6e6df03b82952af
momo56710 <mohamedmouh567@gmail.com>
2024-04-20 19:36:34 +0100

    fixing stats text

--- app/components/statsCard.js
+++ app/components/statsCard.js
@@ -6,7 +6,7 @@ export default function StatsCard({ icon, number, disc }) {
     return (
         <div className='flex flex-col justify-center items-center font-bold gap-2'>
             <img src={icon.src} className='max-md:h-[80px] md:h-[100px]' alt='dollar sign icon' />
-            <p className='max-md:text-[1.5em] bg-gradient-to-r from-[#1F73D4] via-[#7C39D4] to-[#1F73D4] bg-clip-text text-transparent'>{number}</p>
+            <p className='max-md:text-[1.5em] md:text-[2em] bg-gradient-to-r from-[#1F73D4] via-[#7C39D4] to-[#1F73D4] bg-clip-text text-transparent'>{number}</p>
             <p className='text-[#858694]'>{disc}</p>
         </div>
     )
