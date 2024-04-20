ac6fba8df775cb55209422471782a7f421bba343
momo56710 <mohamedmouh567@gmail.com>
2024-04-20 11:40:43 +0100

    minor change

--- app/components/metricsCard.js
+++ app/components/metricsCard.js
@@ -4,7 +4,7 @@ export default function MetricsCard({ number, title }) {
     return (
         <div className='text-center'>
             <p className='text-[3em] text-[#573CFF]'>{number}</p>
-            <p className='text-[0.8em] text-[#9b9b9c]'>{title}</p>
+            <p className='text-[0.9em] text-[#9b9b9c]'>{title}</p>
         </div>
     )
 }
