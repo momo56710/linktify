a56e372ec101ecb2164c1a1e3965509cd3d37660
momo56710 <mohamedmouh567@gmail.com>
2024-10-28 22:04:11 +0100

    fixing issue

--- app/projects/[project]/page.js
+++ app/projects/[project]/page.js
@@ -1,166 +1,212 @@
-'use client'
-import { usePathname } from 'next/navigation'
-import React, { useRef, useEffect, useState } from 'react'
+"use client";
+import { usePathname } from "next/navigation";
+import React, { useRef, useEffect, useState } from "react";
 import { fetchDataFromFireStore } from "@/utils/startups";
-import axios from 'axios';
-import { useRouter } from 'next/navigation';
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import { ImCross } from "react-icons/im";
-import Popup from 'reactjs-popup';
-import { auth, db } from '@/utils/firebase'
-import { toast } from 'react-toastify';
+import Popup from "reactjs-popup";
+import { auth, db } from "@/utils/firebase";
+import { toast } from "react-toastify";
 import ProgressBar from "@ramonak/react-progress-bar";
-import { doc, updateDoc } from 'firebase/firestore';
-import { Loader } from '@/app/components/loader';
-import Loading from '../../components/loading'  
+import { doc, updateDoc } from "firebase/firestore";
+import { Loader } from "@/app/components/loader";
+import Loading from "../../components/loading";
 export default function Page() {
-    const router = useRouter()
-    var pathName = usePathname();
-    const [backingAmount, setBackingAmount] = useState(0)
-    const [user, setUser] = useState('')
-    const [startups, setStartups] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    const [loading, setLoading] = useState(false)
-    const [barWidth, setBarWidth] = useState(0)
-    const progress = useRef();
-    useEffect(() => {
-        async function fetchData() {
-            const data = await fetchDataFromFireStore()
-            setStartups(data)
-            setIsLoading(false)
-        }
-        const unsubscribe = auth.onAuthStateChanged((fetchedUser) => {
-            setUser(fetchedUser);
-            // Update user state on auth state change
-        });
-        fetchData()
-        progress.current ? setBarWidth(progress.current.offsetWidth) : setBarWidth(0)
-    }, [progress.current])
-
-    if (isLoading) { return <Loading /> }
-    //getting startup
-    let startup
-
-    pathName = pathName.replace('/projects/', '')
-    startups.map(e => (e.title.replaceAll(' ', '-').toLowerCase() == pathName ? startup = e : ''))
-
-    // getting funding percentage
-    const percentage = Math.trunc(Number(startup.funded) * 100 / Number(startup.goal))
-
-    // comapring percentage to progress bar
-    const handleBacker = async () => {
-        setLoading(true)
-        
-        startup.backers.push({ backer: { name: user.displayName, email: user.email }, amount: backingAmount })
-        startup.funded = Number(startup.funded) + Number(backingAmount)
-      
-        try {
-            const docRef = doc(db, 'startups', startup.id);
-            await updateDoc(docRef, startup);
-            slickPayTest(backingAmount)
-     
-        } catch (error) {
-            
-        }
+  const router = useRouter();
+  var pathName = usePathname();
+  const [backingAmount, setBackingAmount] = useState(0);
+  const [user, setUser] = useState("");
+  const [refresh, setRefresh] = useState(false);
+  const [startups, setStartups] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [barWidth, setBarWidth] = useState(0);
+  const progress = useRef();
+  useEffect(() => {
+    async function fetchData() {
+      const data = await fetchDataFromFireStore();
+      setStartups(data);
+      setIsLoading(false);
     }
-    const slickPayTest = (amount) => {
-        axios.post(process.env.NEXT_PUBLIC_SLICK_PAY_API_URL, {
-            "amount": amount,
-            "url": "",
-        }, {
-            headers: {
-                "Accept": 'application/json',
-                "Authorization": `Bearer ${process.env.NEXT_PUBLIC_SLICK_PAY_TOKEN}`
-            }
-        })
-            .then((result) => {
-                let response = result.data;
-                setLoading(false)
-                router.push(response.url)
+    const unsubscribe = auth.onAuthStateChanged((fetchedUser) => {
+      setUser(fetchedUser);
+      // Update user state on auth state change
+    });
+    fetchData();
+    progress.current
+      ? setBarWidth(progress.current.offsetWidth)
+      : setBarWidth(0);
+  }, [progress.current , ]);
 
+  if (isLoading) {
+    return <Loading />;
+  }
+  //getting startup
+  let startup;
 
-            }).catch((error) => {
+  pathName = pathName.replace("/projects/", "");
+  startups.map((e) =>
+    e.title.replaceAll(" ", "-").toLowerCase() == pathName ? (startup = e) : ""
+  );
 
-            });
-    }
+  // getting funding percentage
+  const percentage = Math.trunc(
+    (Number(startup.funded) * 100) / Number(startup.goal)
+  );
 
+  // comapring percentage to progress bar
+  const handleBacker = async (close) => {
+    startup.backers.push({
+      backer: { name: user.displayName, email: user.email },
+      amount: backingAmount,
+    });
+    startup.funded = Number(startup.funded) + Number(backingAmount);
 
-    // caluculating remaining days
-    const targetDate = new Date(startup.deadline).toISOString();
+    try {
+      const docRef = doc(db, "startups", startup.id);
+      await updateDoc(docRef, startup);
+      toast("thank you");
+      close();
+      setRefresh(!refresh)
+    } catch (error) {}
+  };
 
-    const now = new Date().getTime()
-    const targetTime = new Date(targetDate).getTime()
-    const timeRemaining = targetTime - now
-    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24)) + 1;
-    return (
+  // caluculating remaining days
+  const targetDate = new Date(startup.deadline).toISOString();
 
-        <div className='max-w-[1600px] m-auto my-10 max-md:text-[12px] md:p-10'>
-            <p className='text-[#2271B9] text-[3em] text-center font-bold'>{startup.title}</p>
-            <div className='bg-white flex flex-col divide-y  md:p-10 max-md:pb-6 mt-5 md:rounded-2xl'>
-                <div className='flex gap-10 max-md:flex-col mb-8'>
-                    <img src={startup.logo} className='md:w-[50%] max-md:m-auto' />
-                    <div className='md:flex-1 my-auto'>
-                        <div className='m-auto max-md:mx-5'>
-                            <p className='text-center mb-4 font-bold text-[2em] max-md:text-[1.5em]'>Stats</p>
-                            <ProgressBar completed={percentage} customLabel=' ' className="h-5 relative max-w-[100%] rounded-[5px] overflow-hidden mr-10" bgColor='#2271B9' />
+  const now = new Date().getTime();
+  const targetTime = new Date(targetDate).getTime();
+  const timeRemaining = targetTime - now;
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24)) + 1;
+  return (
+    <div className="max-w-[1600px] m-auto my-10 max-md:text-[12px] md:p-10">
+      <p className="text-[#2271B9] text-[3em] text-center font-bold">
+        {startup.title}
+      </p>
+      <div className="bg-white flex flex-col divide-y  md:p-10 max-md:pb-6 mt-5 md:rounded-2xl">
+        <div className="flex gap-10 max-md:flex-col mb-8">
+          <img src={startup.logo} className="md:w-[50%] max-md:m-auto" />
+          <div className="md:flex-1 my-auto">
+            <div className="m-auto max-md:mx-5">
+              <p className="text-center mb-4 font-bold text-[2em] max-md:text-[1.5em]">
+                Stats
+              </p>
+              <ProgressBar
+                completed={percentage}
+                customLabel=" "
+                className="h-5 relative max-w-[100%] rounded-[5px] overflow-hidden mr-10"
+                bgColor="#2271B9"
+              />
 
-                            <p className='mb-4 text-[1.5em]'> <span className='font-bold'>{percentage}%</span> of the goal</p>
-                            <p className='text-[1.5em] font-bold text-[#2271B9]'>{startup.funded.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}DZD</p>
-                            <p>pledged of {startup.goal.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",").toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}DZD goal</p>
-                            <p className='mt-5 text-[1.5em] font-bold'>{startup.backers.length}</p>
-                            <p>backers</p>
-                            <p className='mt-5 text-[1.5em] font-bold'>{days}</p>
-                            <p>days to go</p>
-                            <div className='flex gap-4 justify-center items-center mt-5 max-md:flex-col'>
-                                {
-                                    user ? <Popup
-                                        trigger={<p className='bg-[#000218] cursor-pointer text-white px-5 py-3 font-bold rounded-full uppercase max-md:w-full text-center'>Back this project</p>}
-                                        modal
-                                        nested
-                                    >
-                                        {close => (
-                                            <div className="modal p-5">
-                                                <button className="close" onClick={close}>
-                                                    <ImCross className='text-[#2271B9]' />
-                                                </button>
-                                                <div className="header">Back this project</div>
-                                                <div className="content w-full flex flex-col gap-5">
-                                                    <input type='number' placeholder='amount in DZD' className='border bg-transparent p-3 rounded-[10px] w-full' onChange={(e) => { setBackingAmount(e.target.value) }} />
-                                                    <p className='bg-[#2271B9] cursor-pointer  hover:bg-blue-600 text-white gird place-content-center p-2 rounded-md mb-2 text-center flex items-center content-center' onClick={() => { backingAmount < 1000 ? toast("backing amount can't be less than 1000DA") : handleBacker() }}>
-                                                        {loading ? <Loader /> : 'continue to checkout'}</p>
-                                                </div>
-
-                                            </div>
-                                        )}
-                                    </Popup> : <p className='bg-[#000218] cursor-pointer text-white px-5 py-3 font-bold rounded-full uppercase max-md:w-full text-center' onClick={() => { toast('you need to be logged in') }}>Back this project</p>
-
-                                }
-
-                                <p className='bg-[#E7EBF2] text-[#000218] px-5 py-3 font-bold rounded-full uppercase max-md:w-full text-center'>share</p>
-                            </div>
+              <p className="mb-4 text-[1.5em]">
+                {" "}
+                <span className="font-bold">{percentage}%</span> of the goal
+              </p>
+              <p className="text-[1.5em] font-bold text-[#2271B9]">
+                {startup.funded
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                DZD
+              </p>
+              <p>
+                pledged of{" "}
+                {startup.goal
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                DZD goal
+              </p>
+              <p className="mt-5 text-[1.5em] font-bold">
+                {startup.backers.length}
+              </p>
+              <p>backers</p>
+              <p className="mt-5 text-[1.5em] font-bold">{days}</p>
+              <p>days to go</p>
+              <div className="flex gap-4 justify-center items-center mt-5 max-md:flex-col">
+                {user ? (
+                  <Popup
+                    trigger={
+                      <p className="bg-[#000218] cursor-pointer text-white px-5 py-3 font-bold rounded-full uppercase max-md:w-full text-center">
+                        Back this project
+                      </p>
+                    }
+                    modal
+                    nested
+                  >
+                    {(close) => (
+                      <div className="modal p-5">
+                        <button className="close" onClick={close}>
+                          <ImCross className="text-[#2271B9]" />
+                        </button>
+                        <div className="header">Back this project</div>
+                        <div className="content w-full flex flex-col gap-5">
+                          <input
+                            type="number"
+                            placeholder="amount in DZD"
+                            className="border bg-transparent p-3 rounded-[10px] w-full"
+                            onChange={(e) => {
+                              setBackingAmount(e.target.value);
+                            }}
+                          />
+                          <p
+                            className="bg-[#2271B9] cursor-pointer  hover:bg-blue-600 text-white gird place-content-center p-2 rounded-md mb-2 text-center flex items-center content-center"
+                            onClick={() => {
+                              backingAmount < 1000
+                                ? toast(
+                                    "backing amount can't be less than 1000DA"
+                                  )
+                                : handleBacker(close);
+                            }}
+                          >
+                            {"Confirm"}
+                          </p>
                         </div>
+                      </div>
+                    )}
+                  </Popup>
+                ) : (
+                  <p
+                    className="bg-[#000218] cursor-pointer text-white px-5 py-3 font-bold rounded-full uppercase max-md:w-full text-center"
+                    onClick={() => {
+                      toast("you need to be logged in");
+                    }}
+                  >
+                    Back this project
+                  </p>
+                )}
 
-                    </div>
-
-                </div>
-
+                <p className="bg-[#E7EBF2] text-[#000218] px-5 py-3 font-bold rounded-full uppercase max-md:w-full text-center">
+                  share
+                </p>
+              </div>
             </div>
-            <p className='text-center text-[6em] my-10 font-bold text-[#001623]'>our story</p>
-            <div className='flex flex-col gap-[4em] m-10  max-md:items-center'>
-                {
-                    startup.stories.map((e, i) => (
-                        <div key={i} className='flex max-md:flex-col  gap-10 p-10 rounded-2xl bg-white md:divide-x max-md:divide-y max-md:items-center'>
-                            <img src={e.cover} className='max-w-[300px] rounded-2xl aspect-square' />
-                            <div className='grid p-10'>
-                                <p className='font-bold text-[2em]'>{e.title}</p>
-                                <p>{e.disc}</p>
-                            </div>
-                        </div>)
-                    )
-                }
-            </div>
-
-
-
+          </div>
         </div>
-    )
+      </div>
+      <p className="text-center text-[6em] my-10 font-bold text-[#001623]">
+        our story
+      </p>
+      <div className="flex flex-col gap-[4em] m-10  max-md:items-center">
+        {startup.stories.map((e, i) => (
+          <div
+            key={i}
+            className="flex max-md:flex-col  gap-10 p-10 rounded-2xl bg-white md:divide-x max-md:divide-y max-md:items-center"
+          >
+            <img
+              src={e.cover}
+              className="max-w-[300px] rounded-2xl aspect-square"
+            />
+            <div className="grid p-10">
+              <p className="font-bold text-[2em]">{e.title}</p>
+              <p>{e.disc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
--- app/signup/page.js
+++ app/signup/page.js
@@ -1,84 +1,121 @@
-'use client'
-import Link from 'next/link'
-import React, { useState } from 'react'
-import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth'
-import { auth } from '@/utils/firebase'
-import { useRouter } from 'next/navigation'
-import {  updateProfile } from 'firebase/auth'
-import { Loader } from '../components/loader'
-import { toast } from 'react-toastify'
+"use client";
+import Link from "next/link";
+import React, { useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "@/utils/firebase";
+import { useRouter } from "next/navigation";
+import { updateProfile } from "firebase/auth";
+import { Loader } from "../components/loader";
+import { toast } from "react-toastify";
 export default function Page() {
-    const router = useRouter()
-    const [isLoading, setIsLoading] = useState(false);
-    const [name, setName] = useState('')
-    const [profession, setProffession] = useState('')
-    const [warning, setWarning] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [cnfPassword, setCnfPassword] = useState('')
-    const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth)
-
-    const handleSingUp = async () => {
-        if (name == '' || email == '' || password == '' || cnfPassword == '') {
-            setWarning("don't leave inputs empty")
-            toast("Don't leave inputs empty")
-        }
-        else if (password.length < 6) {
-            setWarning('password must be more than 6 char')
-            toast('Password must be more than 6 characters')
-        }
-        else if (!email.includes("@") || !email.includes(".") || !email.length > 2) {
-            setWarning('enter valid email')
-            toast('Enter a valid email')
-        }
-        else if (password != cnfPassword) {
-            setWarning("password doesn't match")
-            toast("Passwords don't match")
-        }
-        else {
-            setIsLoading(true)
-            try {
-                const res = await createUserWithEmailAndPassword(email, password)
-                const user = res.user
-                await updateProfile(user, { displayName: name, photoURL: profession })
-                setEmail('')
-                setPassword('')
-                setName('')
-                toast(`${name} created!`)
-                router.push('/login')
-                setIsLoading(false)
-            } catch (e) {
-                document.querySelectorAll('input').forEach(e => e.value = '')
-                setWarning('email invalid or used before')
-                toast('Email invalid or used before')
-                setIsLoading(false)
-
-
-            }
-        }
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [profession, setProffession] = useState("");
+  const [warning, setWarning] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cnfPassword, setCnfPassword] = useState("");
+  const [createUserWithEmailAndPassword] =
+    useCreateUserWithEmailAndPassword(auth);
+  console.log("g");
+  const handleSingUp = async () => {
+    if (name == "" || email == "" || password == "" || cnfPassword == "") {
+      setWarning("don't leave inputs empty");
+      toast("Don't leave inputs empty");
+    } else if (password.length < 6) {
+      setWarning("password must be more than 6 char");
+      toast("Password must be more than 6 characters");
+    } else if (
+      !email.includes("@") ||
+      !email.includes(".") ||
+      !email.length > 2
+    ) {
+      setWarning("enter valid email");
+      toast("Enter a valid email");
+    } else if (password != cnfPassword) {
+      setWarning("password doesn't match");
+      toast("Passwords don't match");
+    } else {
+      setIsLoading(true);
+      try {
+        const res = await createUserWithEmailAndPassword(email, password);
+        const user = res.user;
+        await updateProfile(user, { displayName: name, photoURL: profession });
+        setEmail("");
+        setPassword("");
+        setName("");
+        toast(`${name} created!`);
+        router.push("/login");
+        console.log(res);
+        setIsLoading(false);
+      } catch (e) {
+        console.log(e);
+        document.querySelectorAll("input").forEach((e) => (e.value = ""));
+        setWarning("email invalid or used before");
+        toast("Email invalid or used before");
+        setIsLoading(false);
+      }
     }
-   
-    return (
-        <div className='flex max-md:flex-col max-md:divide-y justify-center items-center min-h-[70vh] m-10 p-10 bg-white rounded-[20px] md:divide-x'>
-            <p className='text-[#19154E] md:w-[30vw] flex-1 justify-self-center max-md:pb-10 text-center font-bold text-[3em]'>Sign Up</p>
-            <div className='  md:w-[30vw] flex-1 max-md:w-[70vw] md:pl-[5em] max-md:pt-10 '>
-                <div className='flex flex-col gap-5 max-w-[30em] m-auto'>
-                    <input type='text' placeholder='name' className='bg-[#fcfbfb] p-3 rounded-[10px] ' onChange={e => setName(e.target.value)} />
-                    <input type='text' placeholder='profession' className='bg-[#fcfbfb] p-3 rounded-[10px] ' onChange={e => setProffession(e.target.value)} />
-                    <input type='email' placeholder='email' className='bg-[#fcfbfb] p-3 rounded-[10px]' onChange={e => setEmail(e.target.value)} />
-                    <input type='password' placeholder='password' className='bg-[#fcfbfb] p-3 rounded-[10px]' onChange={e => setPassword(e.target.value)} />
-                    <input type='password' placeholder='confirm password' className='bg-[#fcfbfb] p-3 rounded-[10px]' onChange={e => setCnfPassword(e.target.value)} />
-                    <p className='bg-[#2271B9] text-white text-center cursor-pointer rounded-md py-2 grid place-content-center' onClick={() => handleSingUp()}>{isLoading ? <Loader /> : 'Sign up'}</p>
-                    <p className='text-red-600'>{warning}</p>
-                    <hr />
-                    <div>
-                        <p>have an account ? <Link href={'/login'}><span className='text-[#2271B9] hover:underline hover:cursor-pointer'>Log in</span></Link> </p>
-                    </div></div>
+  };
 
-            </div>
-
-
-
+  return (
+    <div className="flex max-md:flex-col max-md:divide-y justify-center items-center min-h-[70vh] m-10 p-10 bg-white rounded-[20px] md:divide-x">
+      <p className="text-[#19154E] md:w-[30vw] flex-1 justify-self-center max-md:pb-10 text-center font-bold text-[3em]">
+        Sign Up
+      </p>
+      <div className="  md:w-[30vw] flex-1 max-md:w-[70vw] md:pl-[5em] max-md:pt-10 ">
+        <div className="flex flex-col gap-5 max-w-[30em] m-auto">
+          <input
+            type="text"
+            placeholder="name"
+            className="bg-[#fcfbfb] p-3 rounded-[10px] "
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="profession"
+            className="bg-[#fcfbfb] p-3 rounded-[10px] "
+            onChange={(e) => setProffession(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            className="bg-[#fcfbfb] p-3 rounded-[10px]"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            className="bg-[#fcfbfb] p-3 rounded-[10px]"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="confirm password"
+            className="bg-[#fcfbfb] p-3 rounded-[10px]"
+            onChange={(e) => setCnfPassword(e.target.value)}
+          />
+          <p
+            className="bg-[#2271B9] text-white text-center cursor-pointer rounded-md py-2 grid place-content-center"
+            onClick={() => handleSingUp()}
+          >
+            {isLoading ? <Loader /> : "Sign up"}
+          </p>
+          <p className="text-red-600">{warning}</p>
+          <hr />
+          <div>
+            <p>
+              have an account ?{" "}
+              <Link href={"/login"}>
+                <span className="text-[#2271B9] hover:underline hover:cursor-pointer">
+                  Log in
+                </span>
+              </Link>{" "}
+            </p>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
